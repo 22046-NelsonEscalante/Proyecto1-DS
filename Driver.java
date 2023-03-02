7bd402e298cd445d16d7fb3565bb4103fcae033b
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-03-02 11:17:22 -0600

    Started filling the classes

--- Driver.java
+++ Driver.java
@@ -1,3 +1,23 @@
+import java.util.Scanner;
+
 public class Driver {
     
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            
+
+            if (writeLine(in).equals("exit")) {
+                System.exit(0);
+            }
+        }
+
+    }
+
+    public static String writeLine(Scanner in) {
+        String response = in.nextLine();
+        return response;
+    }
+
 }
--- Reader.java
+++ Reader.java
@@ -1,3 +1,12 @@
 public class Reader {
-    
+
+    Calculator calc = new Calculator();
+
+    public int operate(String function) {
+
+        int result = calc.calculate(op1, op2, operation);
+
+        return result;
+    }
+
 }
