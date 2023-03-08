519d304c8a6ba8a2f14ffd1e571fef0ab1ec5e42
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-03-08 11:54:28 -0600

    Restructuring the program

--- Reader.java
+++ Reader.java
@@ -1,17 +1,8 @@
 public class Reader {
 
-    Calculator calc = new Calculator();
-
-    public int operate(String function) {
-
-        int result = calc.calculate(op1, op2, operation);
-
-        return result;
-    }
-
     public void read(String input) {
         switch (input) {
-            
+
         }
     }
 
