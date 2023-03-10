da4f61566a4d038e12459ceed98320c80f280ace
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-03-10 07:40:02 -0600

    Working on the reader. The driver should be finished now

--- Reader.java
+++ Reader.java
@@ -1,7 +1,50 @@
-public class Reader {
+import java.util.Stack;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-    public void read(String input) {
-        switch (input) {
+public class Reader {
+    Memory mem = new Memory();
+
+    public ArrayList<String> read(String path) throws Exception {
+        ArrayList<String> file = new ArrayList<>();
+
+            try {
+                BufferedReader reader = new BufferedReader(new FileReader(path));
+                String line = reader.readLine();
+                while (line != null) {
+                    System.out.println(line);
+                    file.add(line);
+                    line = reader.readLine();
+                }
+                reader.close();
+                return file;
+            } catch (Exception e) {
+                throw new Exception();
+            }
+
+    }
+
+    public void execute(ArrayList<String> file) {
+        //System.out.println(file);
+
+        for (String s: file) {
+            //System.out.println(s);
+
+            String[] words;
+            words = s.split(" ");
+
+            switch (words[0]) {
+                case "(print":
+                    String toPrint = "";
+                    ArrayList<String> wordsToPrint = new ArrayList<>();
+                    for (int i = 1; i < words.length; i++) {
+                        words[i] = words[i].replace("\"", "");
+                        wordsToPrint.add(words[i]);
+                    }
+                    toPrint = String.join(" ", wordsToPrint);
+                    System.out.println(toPrint);
+            }
 
         }
     }
