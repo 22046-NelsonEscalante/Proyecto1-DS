cba919d152a13fb12392abb6b294f48e6bd48f1b
22046-NelsonEscalante <esc22046@uvg.edu.gt>
2023-03-16 21:31:36 -0600

    Trying to fix Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -10,62 +10,7 @@ public class Calculator {
     private Stack<ArrayList<String>> pila = new Stack<>();
     private boolean chapuzS = false;
 
-<<<<<<< HEAD
     public float calculate(String op) {
-        
-        String[] operandos = op.split("");
-        
-        ArrayList<String> chapuz = new ArrayList<>();
-        pila.push(chapuz);
-
-        for (String string : operandos) {
-                if(string.equals("(")) {
-
-                    ArrayList<String> operacion = new ArrayList<>();
-
-
-                    pila.push(operacion);
-
-                } else {
-
-                    if(string.equals("+")) {
-                        pila.peek().add("+");
-        
-                    } else if(string.equals("-")) {
-                        pila.peek().add("-");
-        
-                    } else if(string.equals("*")) {
-                        pila.peek().add("*");
-                    
-                    } else if(string.equals("/")) {
-                        pila.peek().add("/");
-        
-                    } else if(string.equals(")")){
-
-                        ArrayList<String> evaluateArrayList = pila.pop();
-
-                        sign = evaluateArrayList.get(0); 
-                        op1 = Float.valueOf(evaluateArrayList.get(1));
-                        op2 = Float.valueOf(evaluateArrayList.get(2));
-        
-                        if(sign.equals("+")) {
-                            res = op1 + op2;
-        
-                        } else if(sign.equals("-")) {
-                            res = op1 - op2;
-        
-                        } else if(sign.equals("*")) {
-                            res = op1 * op2;
-        
-                        } else if(sign.equals("/")) {
-                            res = op1 / op2;
-        
-                        }
-                        
-                        pila.peek().add("" + res);
-                    
-=======
-    public int calculate(String op) {
 
         try {
             String[] operandos = op.split("\\s+");
@@ -81,7 +26,6 @@ public class Calculator {
     
                         pila.push(operacion);
     
->>>>>>> 2d9d6f035859df5a81d12e0f3e179808fc459ba8
                     } else {
     
                         if(string.equals("+")) {
@@ -101,8 +45,8 @@ public class Calculator {
                             ArrayList<String> evaluateArrayList = pila.pop();
     
                             sign = evaluateArrayList.get(0); 
-                            op1 = Integer.parseInt(evaluateArrayList.get(1));
-                            op2 = Integer.parseInt(evaluateArrayList.get(2));
+                            op1 = Float.parseFloat(evaluateArrayList.get(1));
+                            op2 = Float.parseFloat(evaluateArrayList.get(2));
             
                             if(sign.equals("+")) {
                                 res = op1 + op2;
